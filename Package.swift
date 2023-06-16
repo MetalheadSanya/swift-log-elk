7737f5e86a1967e168abc6ca99af85e1013c8d6e
Alexandr Zalutkiy <metalhead.sanya@gmail.com>
2023-06-16 11:19:02 +0300

    Rewrite uploading to async/await

--- Package.swift
+++ Package.swift
@@ -13,14 +13,12 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", .upToNextMinor(from: "1.4.0")),
-        .package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMinor(from: "1.5.0"))
     ],
     targets: [
         .target(
             name: "LoggingELK",
             dependencies: [
                 .product(name: "Logging", package: "swift-log"),
-                .product(name: "AsyncHTTPClient", package: "async-http-client")
             ]
         ),
         .testTarget(
